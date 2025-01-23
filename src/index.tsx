ada76afa7b87297f52c34060e8b3031e62dff701
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-01-23 14:25:40 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -388,7 +388,26 @@ stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
 
       args,
     }}/>, (...args:any) => <Elements.Pressable3 pass={{
+          elementProperties: [(...args:any) => <Elements.Pressable3 pass={{
           elementProperties: [{}],
+
+          styles: [
+              {
+                backgroundColor: 'blue',
+                borderRadius: 20,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }
+              ],
+
+          childrenItems: [()=><></>],
+
+          pressableFunctions: [()=>console.log("Você Clicou!")],
+
+          args,
+        }}/>],
 
           styles: [
               {
