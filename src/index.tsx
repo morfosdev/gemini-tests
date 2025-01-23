e3e587e5d154a6460d933b61cd5e79325124fc07
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-01-23 09:11:49 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@
           import { tools } from './tools';
 
           // ---------- set Caps Inputs
-          const currRoute = 'a2';
+          const currRoute = 'a1';
 
           let args: any = [];
 
