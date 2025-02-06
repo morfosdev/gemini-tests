328f958a9f3001c2bac720eea9b499879c1c1043
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-02-06 13:10:33 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -417,6 +417,22 @@ stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
 
           args,
 
+        }}/>, 
+        (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            {}
+          ],
+
+          arrStyles: [
+            { color: 'black', fontSize: 12, }
+          ],
+
+          children: [
+            "Escreva..."
+          ],
+
+          args,
+
         }}/>, (...args:any) => <Elements.Text pass={{
           arrProps: [
             {}
