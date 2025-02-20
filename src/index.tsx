9d63b1e060bb2f91f326e698523c557335f08a7d
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-02-20 17:22:57 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -370,6 +370,7 @@ stls.justifyContent({ pass: { arrayValue: ['center'] }}), stls.alignItems({ pass
             args,
           }}/>
         , 
+        
 
     (...args:any) => <Elements.ImageBox pass={{
       elementsProperties:[{}],
@@ -386,7 +387,19 @@ stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
         }})],
 
       args,
-    }}/>],
+    }}/>, (...args:any) => <Elements.Pressable3 pass={{
+          elementProperties: [{}],
+
+          styles: [stls.backgroundColor({ pass: { arrayValue: [jsvals.j8({pass: {
+          propertieValues: "purple""
+        }})] }})],
+
+          childrenItems: [()=><></>],
+
+          pressableFunctions: [()=>console.log("Você Clicou!")],
+
+          args,
+        }}/>],
 
             args,
           }}/>
