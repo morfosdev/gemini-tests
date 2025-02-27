f548d3c4fdfb06367f48037d385d7889397fe813
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-02-27 16:23:02 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -370,7 +370,6 @@ stls.justifyContent({ pass: { arrayValue: ['center'] }}), stls.alignItems({ pass
             args,
           }}/>
         , 
-        
 
     (...args:any) => <Elements.ImageBox pass={{
       elementsProperties:[{}],
@@ -387,26 +386,7 @@ stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
         }})],
 
       args,
-    }}/>, (...args:any) => <Elements.Text pass={{
-          arrProps: [
-            {}
-          ],
-
-          arrStyles: [
-            stls.color({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: ""
-        }})] }})
-          ],
-
-          children: [
-            jsvals.j8({pass: {
-          propertieValues: "texto color"
-        }})
-          ],
-
-          args,
-
-        }}/>],
+    }}/>],
 
             args,
           }}/>
